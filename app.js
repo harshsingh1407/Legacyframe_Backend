394d167bf9ebf9929f95be8f2ad8f924211c4123
Harsh Singh <harshsingh95941@gmail.com>
2026-05-13 22:09:21 +0530

    Fix

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://legacyframe.vercel.app"
+  ]
+}));
 app.use(express.json());
 
 // Serve static files from uploads folder
